4e67966804239f18f4f2075eb3444ce86cb068a7
Natasha Kapitonova <nat-kpt@yandex.ru>
2024-12-22 18:35:11 +0300

    basic application

---
+++ rpn/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/NatalieIsupova/rpn/internal/application"
+)
+
+func main() {
+	app := application.New()
+	app.Run()
+}
---
+++ rpn/go.mod
@@ -0,0 +1,3 @@
+module github.com/NatalieIsupova/rpn
+
+go 1.23.3
